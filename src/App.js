f0125cc7af1517aa2a67adef6736c06ea552f3e9
Anastasija Lagodzinska <a.lagodzinska@outlook.com>
2024-06-18 13:17:50 -0700

    post page sketch

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import RestorePost from "./routes/RestorePost";
 import Layout from "./Layout";
 import MainPage from "./routes/MainPage";
 import PostsPage from "./routes/PostsPage";
+import Test from "./routes/UI_postsFIX";
 
 function App() {
   const router = createBrowserRouter([
@@ -30,6 +31,10 @@ function App() {
         {
           path: "/posts",
           element: <PostsPage />,
+        },
+        {
+          path: "/test",
+          element: <Test />,
         },
         {
           element: <PrivateRouteLoggedOut />,
