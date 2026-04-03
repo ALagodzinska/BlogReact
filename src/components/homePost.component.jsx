1eedd5efac30fd54f625d76796a9a962bff017f3
Anastasija Lagodzinska <alagodzi@students.cs.ubc.ca>
2026-04-02 20:48:57 -0700

    Fixed logged in User home post style

--- src/components/homePost.component.jsx
+++ src/components/homePost.component.jsx
@@ -9,6 +9,8 @@ import StarOutlineRoundedIcon from "@mui/icons-material/StarOutlineRounded";
 import GradeRoundedIcon from "@mui/icons-material/GradeRounded";
 import FeaturePopup from "./featurePopup.component";
 import styles from "../styles/components/homePost.styles";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import DeleteOutlineRoundedIcon from "@mui/icons-material/DeleteOutlineRounded";
 
 function HomePost({ post, refreshPostsAction }) {
   const [user] = useContext(UserContext);
@@ -47,63 +49,83 @@ function HomePost({ post, refreshPostsAction }) {
       />
       <Grid item xs={12} md={8} sx={styles.gridItem}>
         <Stack sx={styles.cardStack}>
-          <Box sx={styles.contentContainer}>
-            <Typography variant="h4" gutterBottom sx={styles.title}>
-              {post.title}
-            </Typography>
-            <Typography variant="caption" sx={styles.date}>
-              {formattedDate}
-            </Typography>
-            <Box
-              component="div"
-              sx={styles.postPreview}
-              dangerouslySetInnerHTML={{ __html: truncate(post.content, 400) }}
-            />
-            <Stack direction="row" alignItems="center" spacing={1.5} sx={styles.actionRow}>
-              <Button
-                variant="contained"
-                component={Link}
-                to={`/${post.blogPostId}`}
-                sx={styles.readMoreButton}
-              >
-                Read More
-              </Button>
-              {user && (
-                <Button component={Link} to={`/edit/${post.blogPostId}`} variant="text" sx={styles.actionButton}>
+          <Stack sx={styles.mainRow}>
+            <Box sx={styles.contentContainer}>
+              <Typography variant="h4" gutterBottom sx={styles.title}>
+                {post.title}
+              </Typography>
+              <Typography variant="caption" sx={styles.date}>
+                {formattedDate}
+              </Typography>
+              <Box
+                component="div"
+                sx={styles.postPreview}
+                dangerouslySetInnerHTML={{ __html: truncate(post.content, 400) }}
+              />
+            </Box>
+            <Box sx={styles.imageColumn}>
+              <Box sx={styles.imageFrame}>
+                <Box
+                  component="img"
+                  src={`/api/Image/PreviewImage?postId=${post.blogPostId}`}
+                  alt={post.title}
+                  sx={styles.image}
+                />
+                <Box sx={styles.authorBadge}>
+                  {post.user}
+                </Box>
+
+                {user &&
+                  (post.isFeatured ? (
+                    <Box title="Featured post" sx={styles.featuredBadge}>
+                      <GradeRoundedIcon sx={styles.featuredBadgeIcon} />
+                      Featured
+                    </Box>
+                  ) : (
+                    <Button
+                      title="Make featured"
+                      onClick={openFeatureWindow}
+                      variant="contained"
+                      startIcon={<StarOutlineRoundedIcon sx={styles.featureButtonIcon} />}
+                      sx={styles.featureButton}
+                    >
+                      Feature
+                    </Button>
+                  ))}
+              </Box>
+            </Box>
+          </Stack>
+          <Stack direction="row" alignItems="center" sx={styles.actionRow}>
+            <Button
+              variant="contained"
+              component={Link}
+              to={`/${post.blogPostId}`}
+              sx={styles.readMoreButton}
+            >
+              Read More
+            </Button>
+            {user && (
+              <Stack direction="row" alignItems="center" sx={styles.adminActionGroup}>
+                <Button
+                  component={Link}
+                  to={`/edit/${post.blogPostId}`}
+                  variant="outlined"
+                  startIcon={<EditOutlinedIcon sx={styles.adminActionIcon} />}
+                  sx={styles.actionButton}
+                >
                   Edit
                 </Button>
-              )}
-              {user && (
-                <Button variant="text" onClick={openDeleteWindow} sx={styles.deleteButton}>
+                <Button
+                  variant="outlined"
+                  onClick={openDeleteWindow}
+                  startIcon={<DeleteOutlineRoundedIcon sx={styles.adminActionIcon} />}
+                  sx={styles.deleteButton}
+                >
                   Delete
                 </Button>
-              )}
-            </Stack>
-          </Box>
-          <Box sx={styles.imageColumn}>
-            <Box sx={styles.imageFrame}>
-              <Box
-                component="img"
-                src={`/api/Image/PreviewImage?postId=${post.blogPostId}`}
-                alt={post.title}
-                sx={styles.image}
-              />
-              <Box sx={styles.authorBadge}>
-                {post.user}
-              </Box>
-
-              {user &&
-                (post.isFeatured ? (
-                  <Box title="Featured post">
-                    <GradeRoundedIcon sx={{ ...styles.featuredIcon, ...styles.featuredIconHover }} />
-                  </Box>
-                ) : (
-                  <Box title="Make featured" onClick={openFeatureWindow}>
-                    <StarOutlineRoundedIcon sx={{ ...styles.featuredIcon, ...styles.makeFeaturedIcon }} />
-                  </Box>
-                ))}
-            </Box>
-          </Box>
+              </Stack>
+            )}
+          </Stack>
         </Stack>
       </Grid>
       <Divider />
